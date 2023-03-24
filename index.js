f5d5d73a660a6b08daaf41dbcdb85617974969e2
Valtteri Andström <valtteri.andstrom@gmail.com>
2023-03-24 13:03:03 +0200

    store players to mongoDB

--- index.js
+++ index.js
@@ -6,15 +6,27 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 
 const { results, players } = require('./utils')
-
+const Player = require('./models/player')
 const url = process.env.MONGODB_URI
+
+mongoose.set('strictQuery', false)
+
+console.log('connecting to', url)
+mongoose.connect(url)
+  .then(result => {    
+    console.log('connected to MongoDB') 
+  })  
+  .catch((error) => {    
+    console.log('error connecting to MongoDB:', 
+    error.message)  
+  })
 
 app.use(cors())
 
 app.use(express.json())
 
-console.log(results)
-console.log(players)
+// console.log(results)
+// console.log(players)
 
 
 app.get('/', (req, res) => {
@@ -27,6 +39,20 @@ app.get('/api/players', (req, res) => {
 
 app.get('/api/results', (req, res) => {
   res.json(results)
+})
+
+app.post('/api/players', (request, response) => {
+  const body = request.body
+  console.log("body ", body)
+  const newPlayer = new Player({
+    name: body.name,
+    guesses: body.guesses
+  })
+
+  newPlayer.save().then(result => {
+    console.log('new player saved!')
+    mongoose.connection.close()
+  })
 })
 
 const PORT = 3001
---
+++ models/player.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose')
+
+const playerSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    ones: {
+        type: Number,
+        default: 0
+    },
+    twos: {
+        type: Number,
+        default: 0
+    },
+    threes: {
+        type: Number,
+        default: 0
+    },
+    first_one: {
+        type: Number,
+        default: 99
+    },
+    first_two: {
+        type: Number,
+        default: 99
+    },
+    first_three: {
+        type: Number,
+        default: 99
+    },
+    guesses: [{
+        match: {
+            type: String,
+            required: true
+        },
+        h: {
+            type: Number,
+            required: true,
+            min: 0
+        },
+        a: {
+            type: Number,
+            required: true,
+            min: 0
+        },
+    }]
+
+})
+
+playerSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Player', playerSchema)
